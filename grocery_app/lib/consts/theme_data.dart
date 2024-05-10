07192245a961e8c4d1eda754809d23fc1d807b3d
Nathan Newyen <nathan@nathannewyen.com>
2024-05-10 18:31:38 -0400

    add home screen and update feature dark mode

---
+++ grocery_app/lib/consts/theme_data.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static ThemeData themeData(bool isDarkTheme, BuildContext context) {
+    return ThemeData(
+        scaffoldBackgroundColor:
+            isDarkTheme ? const Color(0xFF1A1A1A) : const Color(0xFFFFFFFF),
+        primaryColor: Colors.blue,
+        colorScheme: ThemeData().colorScheme.copyWith(
+              secondary: isDarkTheme
+                  ? const Color(0xFF1A1F3C)
+                  : const Color(0xFFE8FDFD),
+              brightness: isDarkTheme ? Brightness.dark : Brightness.light,
+            ),
+        cardColor:
+            isDarkTheme ? const Color(0xFF0A0D2C) : const Color(0xFFF2FDFD),
+        canvasColor: isDarkTheme ? Colors.black : Colors.grey[50],
+        buttonTheme: Theme.of(context).buttonTheme.copyWith(
+              colorScheme: isDarkTheme
+                  ? const ColorScheme.dark()
+                  : const ColorScheme.light(),
+            ));
+  }
+}
